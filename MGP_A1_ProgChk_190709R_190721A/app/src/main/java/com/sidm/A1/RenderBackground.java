5761b1ead150e202dc43510904609a1a96ae76e2
klareity <taonanite2014@gmail.com>
2020-12-12 15:47:23 +0800

    update to renderbackground
    added codes to make it less skeletal, more muscular

--- MGP_A1_ProgChk_190709R_190721A/app/src/main/java/com/sidm/A1/RenderBackground.java
+++ MGP_A1_ProgChk_190709R_190721A/app/src/main/java/com/sidm/A1/RenderBackground.java
@@ -1,12 +1,17 @@
 package com.sidm.A1;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.util.DisplayMetrics;
 import android.view.SurfaceView;
+
 
 public class RenderBackground implements EntityBase {
     private Bitmap bmp = null;
     private boolean isDone = false;
+    private float xPos, yPos, xPos2, yPos2 = 0.0f;
+    int screenWidth, screenHeight;
 
     public static void Create() {
     }
@@ -24,11 +29,19 @@ public class RenderBackground implements EntityBase {
     @Override
     public void Init(SurfaceView _view) {
 
+
+        DisplayMetrics metrics = _view.getResources().getDisplayMetrics();
+        screenWidth = metrics.widthPixels;
+        screenHeight = metrics.heightPixels;
     }
 
     @Override
     public void Update(float _dt) {
+        xPos -= _dt * 500;  // How fast it scrolls moving from right to left.
 
+        if (xPos < - screenWidth){  // xpos
+            xPos = 0;
+        }
     }
 
     @Override
