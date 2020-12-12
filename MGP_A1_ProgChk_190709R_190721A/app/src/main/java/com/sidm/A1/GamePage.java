2142681c111fcd72a5bb7898ebca064f89134106
klareity <taonanite2014@gmail.com>
2020-12-12 21:26:05 +0800

    modfied gamepage related stuff
    it crashes when it runs, the error message points to OnCreate().
    
    I am mongkas

--- MGP_A1_ProgChk_190709R_190721A/app/src/main/java/com/sidm/A1/GamePage.java
+++ MGP_A1_ProgChk_190709R_190721A/app/src/main/java/com/sidm/A1/GamePage.java
@@ -4,14 +4,24 @@ package com.sidm.A1;
 // Create a GamePage is an activity class used to hold the GameView which will have a surfaceview
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.view.SurfaceView;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.view.View.OnClickListener;
 
-public class GamePage extends Activity {
+public class GamePage extends Activity implements OnClickListener, StateBase {
 
     public static GamePage Instance = null;
+
+    //defining buttons
+    private ImageButton moveLeft;
+    private ImageButton moveRight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +29,40 @@ public class GamePage extends Activity {
 
         //To make fullscreen
         requestWindowFeature(Window.FEATURE_NO_TITLE); // Hide titlebar
+
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);  // Hide topbar
 
         Instance = this;
 
         setContentView(new GameView(this)); // Surfaceview = GameView
+
+        moveLeft=(ImageButton)findViewById(R.id.moveLeft);
+        moveLeft.setOnClickListener(this);
+
+        moveRight=(ImageButton)findViewById(R.id.moveRight);
+        moveRight.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        // Intent = action to be performed.
+        // Intent is an object provides runtime binding.
+        // new instance of this object intent
+
+        Intent intent = new Intent();
+
+        if(v==moveLeft)
+        {
+            intent.setClass(this, GamePage.class);
+
+        }
+        if(v==moveRight)
+        {
+            intent.setClass(this, GamePage.class);
+
+        }
+        startActivity(intent);
     }
 
     @Override
@@ -38,5 +77,30 @@ public class GamePage extends Activity {
         return true;
     }
 
+
+    @Override
+    public String GetName() {
+        return "GamePage";
+    }
+
+    @Override
+    public void OnEnter(SurfaceView _view) {
+
+    }
+
+    @Override
+    public void OnExit() {
+
+    }
+
+    @Override
+    public void Render(Canvas _canvas) {
+
+    }
+
+    @Override
+    public void Update(float _dt) {
+
+    }
 }
 
