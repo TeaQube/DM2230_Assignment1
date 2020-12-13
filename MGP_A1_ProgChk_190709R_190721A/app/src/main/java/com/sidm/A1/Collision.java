b8613f32a28dc9f9ef4e9d49a69ac82660880593
klareity <taonanite2014@gmail.com>
2020-12-14 02:05:12 +0800

    aabb thing
    aabb somewhat

--- MGP_A1_ProgChk_190709R_190721A/app/src/main/java/com/sidm/A1/Collision.java
+++ MGP_A1_ProgChk_190709R_190721A/app/src/main/java/com/sidm/A1/Collision.java
@@ -19,4 +19,15 @@ public class Collision {
 
         return true;
     }
+
+    public static boolean AABB(float x1, float x2, float y1, float y2 ,float width1, float height1,float width2,float height2)
+    {
+        if(x1 < x2 + width2 && x1 + width1 > x2 && y1 < y2 + height2 && y1 + height1 > y2)
+        {
+            return true;
+        }
+        return false;
+    }
+
+
 }
