35ea1d2914c58e620a1c65178bb43d42ee05a9bc
kwyub3 <thunderwave.chen@gmail.com>
2021-02-07 04:09:01 +0800

    pausebutton fix

--- MGP_A1_ProgChk_190709R_190721A/app/src/main/java/com/sidm/A1/PauseConfirmDialogFragment.java
+++ MGP_A1_ProgChk_190709R_190721A/app/src/main/java/com/sidm/A1/PauseConfirmDialogFragment.java
@@ -3,12 +3,12 @@ package com.sidm.A1;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 
 import androidx.fragment.app.DialogFragment;
 
-//by qing bing
 public class PauseConfirmDialogFragment extends DialogFragment {
 
     public static boolean IsShown, Paused = false;
@@ -20,20 +20,26 @@ public class PauseConfirmDialogFragment extends DialogFragment {
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = requireActivity().getLayoutInflater();
-        builder.setMessage("Game is paused");
+        builder.setMessage("Game Paused.");
         builder.setView(inflater.inflate(R.layout.dialogbox, null))
                 .setPositiveButton("Resume", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        // User triggered pause
-//                        GameSystem.Instance.SaveEditBegin();
-//                        StateManager.Instance.ChangeState("Mainmenu");
-//                        GameSystem.Instance.SaveEditEnd();
-//                        IsShown = false;
+                        // User cancelled the pause
                         GameSystem.Instance.SetIsPaused(!GameSystem.Instance.GetIsPaused());
                         IsShown = false;
                     }
+                })
+                .setNegativeButton("Quit", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User triggered pause
+                        Intent i = new Intent(getContext(), Mainmenu.class);
+                        getContext().startActivity(i);
+                        StateManager.Instance.ChangeState("Mainmenu");
+                        GameSystem.Instance.SaveEditBegin();
+                        GameSystem.Instance.SaveEditEnd();
+                        IsShown = false;
+                    }
                 });
-
         // Create the AlertDialog object and return it
         return builder.create();
     }
@@ -47,4 +53,5 @@ public class PauseConfirmDialogFragment extends DialogFragment {
     public void onDismiss(DialogInterface dialog) {
         IsShown = false;
     }
+
 }
